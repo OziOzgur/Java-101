254f72e068db8ce6954da2fcef4681e6fc4e96cd
OziOzgur <145540891+OziOzgur@users.noreply.github.com>
2023-10-09 11:45:20 +0300

    Add files via upload

---
+++ AsalSayilar.java
@@ -0,0 +1,20 @@
+package Donguler;
+
+public class AsalSayilar {
+    public static void main(String[] args) {
+
+        for (int i = 2; i <= 100 ; i++) {
+           boolean asalmi = true;
+            for (int j = 2; j < i; j++){
+                if (i % j== 0){
+                    asalmi = false;
+                    break;
+                }
+            }
+            if (asalmi) {
+                System.out.print(i + " ");
+            }
+
+        }
+    }
+}
