6641058bee5fb88913105945a559cb7497daba83
OziOzgur <145540891+OziOzgur@users.noreply.github.com>
2023-10-16 08:48:17 +0300

    Add files via upload

---
+++ PalindromSayi.java
@@ -0,0 +1,28 @@
+package Metotlar;
+
+public class PalindromSayi {
+
+    static boolean isPalindrom (int number){
+        int temp = number, reverseNumber =0, lastNumber;
+        while (temp != 0){
+            System.out.println("=========");
+            System.out.println("Sayı => " + temp);
+            lastNumber = temp % 10;
+            System.out.println("Son Basamak => " + lastNumber);
+            reverseNumber = (reverseNumber * 10) + lastNumber;
+            System.out.println("Yeni Sayı => " + reverseNumber);
+            temp /= 10;
+        }
+        if (number == reverseNumber)
+            return true;
+        else
+            return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrom(9889));
+        // 7 * 10 = 70 + 4 = 74
+        // 74 * 10 = 740 + 2 = 742
+
+    }
+}
